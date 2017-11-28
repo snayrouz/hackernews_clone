3e2613b48f3f135c147242e8d90924d38738be85
snayrouz <samuel.nayrouz@gmail.com>
2017-11-28 15:22:40 -0700

    Add all links query

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -24,6 +24,16 @@ class LinkList extends Component {
     )
   }
 
+const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
 }
 
-export default LinkList
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
