e21734f4a4fdd1b984058b7a15915d84fddc6a7b
snayrouz <samuel.nayrouz@gmail.com>
2017-11-28 15:13:04 -0700

    Modify index.js to compile

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css'
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjak10d0b3q920185m2go8c4r' })
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+)
+registerServiceWorker()
