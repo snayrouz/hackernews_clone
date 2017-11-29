c70a4af673462e6e5df5aaee0310e309058b412c
snayrouz <samuel.nayrouz@gmail.com>
2017-11-28 18:28:57 -0700

    Add auth for login/signup

--- hackernews-react-apollo/src/components/Login.js
+++ hackernews-react-apollo/src/components/Login.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
+
 
 class Login extends Component {
 
@@ -55,8 +58,30 @@ class Login extends Component {
   }
 
   _confirm = async () => {
-    // ... you'll implement this in a bit
+    const { name, email, password } = this.state
+    if (this.state.login) {
+      const result = await this.props.authenticateUserMutation({
+        variables: {
+          email,
+          password
+        }
+      })
+      const { id, token } = result.data.authenticateUser
+      this._saveUserData(id, token)
+    } else {
+      const result = await this.props.signupUserMutation({
+        variables: {
+          name,
+          email,
+          password
+        }
+      })
+      const { id, token } = result.data.signupUser
+      this._saveUserData(id, token)
+    }
+    this.props.history.push(`/`)
   }
+
 
   _saveUserData = (id, token) => {
     localStorage.setItem(GC_USER_ID, id)
@@ -65,4 +90,34 @@ class Login extends Component {
 
 }
 
-export default Login
+const SIGNUP_USER_MUTATION = gql`
+  mutation SignupUserMutation($email: String!, $password: String!, $name: String!) {
+    signupUser(
+      email: $email,
+      password: $password,
+      name: $name
+    ) {
+      id
+      token
+    }
+  }
+`
+
+const AUTHENTICATE_USER_MUTATION = gql`
+  mutation AuthenticateUserMutation($email: String!, $password: String!) {
+    authenticateUser(email: {
+      email: $email,
+      password: $password
+    }) {
+      token
+      user {
+        id
+      }
+    }
+  }
+`
+
+export default compose(
+  graphql(SIGNUP_USER_MUTATION, { name: 'signupUserMutation' }),
+  graphql(AUTHENTICATE_USER_MUTATION, { name: 'authenticateUserMutation' })
+)(Login)
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -8,14 +8,30 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
+import { GC_AUTH_TOKEN } from './constants'
+import { ApolloLink } from 'apollo-client-preset'
 
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjak10d0b3q920185m2go8c4r' })
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 })
+
 
 ReactDOM.render(
   <BrowserRouter>
